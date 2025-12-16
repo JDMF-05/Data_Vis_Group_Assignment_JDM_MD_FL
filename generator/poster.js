2e0bcc58f514379d647643a19949466a910036ed
JDMF-05 <joshuamoshif@gmail.com>
2025-12-16 12:59:47 +0100

    Update poster.js

--- generator/poster.js
+++ generator/poster.js
@@ -19,7 +19,7 @@ const BAR_LEFT_X = 220;
 const BAR_RIGHT_X = 935;
 const BAR_WIDTH = BAR_RIGHT_X - BAR_LEFT_X;
 const BAR_HEIGHT = 52;
-const BAR_PADDING_X = 28;
+const BAR_PADDING_X = 30;
 
 /* =========================
    ROW POSITIONS (ABSOLUTE)
