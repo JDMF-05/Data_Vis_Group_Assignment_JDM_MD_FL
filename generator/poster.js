df2c02e08b184359ca14e70d0773b3ca24080a30
JDMF-05 <joshuamoshif@gmail.com>
2025-12-16 11:49:18 +0100

    Update poster.js

--- generator/poster.js
+++ generator/poster.js
@@ -28,10 +28,10 @@ const BAR_PADDING_X = 18;
 
 const BAR_TOP_Y = [
   625,  // row 1
-  800,  // row 2
-  940,  // row 3
-  1060, // row 4
-  1300  // row 5
+  770,  // row 2
+  900,  // row 3
+  1030, // row 4
+  1160  // row 5
 ];
 
 // 🔧 Title nudges (minor optical correction only)
