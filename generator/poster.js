e3747fd67a414c81600cbdebc87f20d6e81a35b8
JDMF-05 <joshuamoshif@gmail.com>
2025-12-16 13:42:34 +0100

    Update poster.js

--- generator/poster.js
+++ generator/poster.js
@@ -21,7 +21,7 @@ const BAR_WIDTH = BAR_RIGHT_X - BAR_LEFT_X;
 const BAR_HEIGHT = 52;
 
 // Text padding inside bars
-const BAR_PADDING_X = 110;
+const BAR_PADDING_X = 136;
 
 /* =========================
    ROW POSITIONS (ABSOLUTE)
