24647db68dcb022efaedf4af46ae4755d841e15e
JDMF-05 <joshuamoshif@gmail.com>
2026-01-07 11:43:53 +0100

    Update main.js

--- generator/main.js
+++ generator/main.js
@@ -11,40 +11,100 @@ const EASTER_EGGS = [
   {
     trigger: "Banana",
     embedHTML: `
-      <div class="tenor-gif-embed" data-postid="26063026" data-share-method="host" data-aspect-ratio="1.50235" data-width="100%">
-        <a href="https://tenor.com/view/peely-the-shame-meme-cringe-caught-gif-26063026">Peely The Shame GIF</a>
-        from <a href="https://tenor.com/search/peely-gifs">Peely GIFs</a>
+      <div class="tenor-gif-embed"
+           data-postid="26063026"
+           data-share-method="host"
+           data-aspect-ratio="1.50235"
+           data-width="100%">
+        <a href="https://tenor.com/view/peely-the-shame-meme-cringe-caught-gif-26063026">
+          Peely The Shame GIF
+        </a>
+        from
+        <a href="https://tenor.com/search/peely-gifs">
+          Peely GIFs
+        </a>
       </div>
     `
   },
   {
     trigger: "Mattia Dellamonica",
     embedHTML: `
-      <div class="tenor-gif-embed" data-postid="8510554628372069925" data-share-method="host" data-aspect-ratio="0.779116" data-width="100%">
-        <a href="https://tenor.com/view/cat-gun-gif-8510554628372069925">Cat Gun GIF</a>
-        from <a href="https://tenor.com/search/cat+gun-gifs">Cat Gun GIFs</a>
+      <div class="tenor-gif-embed"
+           data-postid="8510554628372069925"
+           data-share-method="host"
+           data-aspect-ratio="0.779116"
+           data-width="100%">
+        <a href="https://tenor.com/view/cat-gun-gif-8510554628372069925">
+          Cat Gun GIF
+        </a>
+        from
+        <a href="https://tenor.com/search/cat+gun-gifs">
+          Cat Gun GIFs
+        </a>
       </div>
     `
   },
   {
     trigger: "Federico Lombardo",
     embedHTML: `
-      <div class="tenor-gif-embed" data-postid="13290601400007972669" data-share-method="host" data-aspect-ratio="1" data-width="100%">
-        <a href="https://tenor.com/view/gundam-mobile-suit-gundam-mcdonald%27s-char-aznable-char-gundam-gif-13290601400007972669">Gundam GIF</a>
-        from <a href="https://tenor.com/search/gundam-gifs">Gundam GIFs</a>
+      <div class="tenor-gif-embed"
+           data-postid="13290601400007972669"
+           data-share-method="host"
+           data-aspect-ratio="1"
+           data-width="100%">
+        <a href="https://tenor.com/view/gundam-mobile-suit-gundam-mcdonald%27s-char-aznable-char-gundam-gif-13290601400007972669">
+          Gundam GIF
+        </a>
+        from
+        <a href="https://tenor.com/search/gundam-gifs">
+          Gundam GIFs
+        </a>
       </div>
     `
   },
   {
     trigger: "Joshua Moshi",
     embedHTML: `
-      <div class="tenor-gif-embed" data-postid="5319769" data-share-method="host" data-aspect-ratio="0.715" data-width="100%">
-        <a href="https://tenor.com/view/futurama-bender-dance-gif-5319769">Bender Dancing GIF</a>
-        from <a href="https://tenor.com/search/futurama-gifs">Futurama GIFs</a>
+      <div class="tenor-gif-embed"
+           data-postid="5319769"
+           data-share-method="host"
+           data-aspect-ratio="0.715"
+           data-width="100%">
+        <a href="https://tenor.com/view/futurama-bender-dance-gif-5319769">
+          Bender Dancing GIF
+        </a>
+        from
+        <a href="https://tenor.com/search/futurama-gifs">
+          Futurama GIFs
+        </a>
+      </div>
+    `
+  },
+
+  /* 🥚 NEW EASTER EGG */
+  {
+    trigger: "The Weekend",
+    embedHTML: `
+      <div class="tenor-gif-embed"
+           data-postid="20662583"
+           data-share-method="host"
+           data-aspect-ratio="1"
+           data-width="100%">
+        <a href="https://tenor.com/view/honeycardi-the-weeknd-abel-abel-tesfaye-gif-20662583">
+          Honeycardi The Weeknd GIF
+        </a>
+        from
+        <a href="https://tenor.com/search/honeycardi-gifs">
+          Honeycardi GIFs
+        </a>
       </div>
     `
   }
 ];
+
+/* =========================
+   TENOR
+========================= */
 
 function loadTenorScript() {
   const old = document.getElementById("tenor-embed-js");
@@ -56,6 +116,10 @@ function loadTenorScript() {
   s.src = "https://tenor.com/embed.js";
   document.body.appendChild(s);
 }
+
+/* =========================
+   CLEAR UI
+========================= */
 
 function clearPosterUI() {
   const canvas = document.getElementById("top5_canvas");
@@ -72,7 +136,6 @@ function clearPosterUI() {
   if (downloadBtn) downloadBtn.style.display = "none";
   if (resetBtn) resetBtn.style.display = "none";
 }
-
 
 function clearEasterEgg() {
   const el = document.getElementById("easter_egg");
